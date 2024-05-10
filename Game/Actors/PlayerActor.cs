136e2e8fde2ab12c9016a471d7fe1106ee9ef207
MohammadMobasher <m.mobasher.z@gmail.com>
2024-05-10 18:02:18 +0330

    Add project files.

---
+++ Game/Actors/PlayerActor.cs
@@ -0,0 +1,46 @@
+﻿using Akka.Actor;
+using Game.Messages;
+
+namespace Game.Actors
+{
+    public class PlayerActor : ReceiveActor
+    {
+        private string PlayerName { get; set; }
+        private int Health { get; set; }
+
+        public PlayerActor(string playerName, int health)
+        {
+
+            PlayerName = playerName;
+            Health = health;
+
+            Console.WriteLine($"{PlayerName} created.");
+
+            // Receives
+            Receive<DisplayPlayerMessage>(_ => Display());
+            Receive<HitPlayerMessage>(message => Hit(message));
+            Receive<ErrorMessage>(message => Error());
+        }
+
+        private void Display()
+        {
+            Console.WriteLine($"{PlayerName} received display message.");
+
+            Console.WriteLine($"{PlayerName} has {Health} health");
+        }
+
+        private void Hit(HitPlayerMessage input)
+        {
+            Console.WriteLine($"{PlayerName} received hit message.");
+
+            Health -= input.Damage;
+        }
+
+        private void Error()
+        {
+            Console.WriteLine($"{PlayerName} received CauseErrorMessage");
+
+            throw new ApplicationException($"Simulated exception in player: {PlayerName}");
+        }
+    }
+}
---
+++ Game/Messages/CreatePlayerGameMessage.cs
@@ -0,0 +1,12 @@
+﻿namespace Game.Messages
+{
+    public class CreatePlayerGameMessage
+    {
+        public string PlayerName { get; set; }
+
+        public CreatePlayerGameMessage(string playerName)
+        {
+            PlayerName = playerName;
+        }
+    }
+}
